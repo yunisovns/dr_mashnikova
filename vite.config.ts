ff4f1aa9aafead027d2626312d383f58ee3509ed
Sinet <nsyunisov@nsc.media>
2025-08-04 19:48:41 +0300

    Добавил gitgub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/dr_mashnikova",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
